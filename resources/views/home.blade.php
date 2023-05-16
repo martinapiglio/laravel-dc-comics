82f60c95de7da3d0919488ec59346ba86199ff85
martinapiglio <martina.piglio@gmail.com>
2023-05-16 13:48:29 +0200

    layouts + partials + img  config data added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,24 +1,5 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layouts/main')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Laravel DC Comics</title>
-
-    <!-- Fonts -->
-    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-    <!-- Styles -->
-    @vite('resources/js/app.js')
-
-</head>
-
-<body>
-
+@section('content')
     ciao
-
-</body>
-
-</html>
+@endsection
